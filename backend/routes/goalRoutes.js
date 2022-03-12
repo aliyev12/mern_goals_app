8081814590ea6082a95fc180de481455bf5c3e8c
Abdul Aliyev <aaa7c4@gmail.com>
2022-03-12 08:09:03 -0500

    Add user controllers

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -6,9 +6,10 @@ const {
   updateGoal,
   deleteGoal,
 } = require("../controllers/goalController");
+const { protect } = require("../middleware/authMiddleware");
 
-router.route("/").get(getGoals).post(setGoals);
+router.route("/").get(protect, getGoals).post(protect, setGoals);
 
-router.route("/:id").put(updateGoal).delete(deleteGoal);
+router.route("/:id").put(protect, updateGoal).delete(protect, deleteGoal);
 
 module.exports = router;
